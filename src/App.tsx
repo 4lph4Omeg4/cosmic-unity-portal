33f8b67d1afc1e15cd5e17c6ed27eb600b33431e
SH4M4NI4K <135929616+4lph4Omeg4@users.noreply.github.com>
2025-08-31 04:18:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { CartProvider } from "@/hooks/useCart";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import NewsletterPopup from "@/components/NewsletterPopup";
+import { NewsletterPopup } from "@/components/NewsletterPopup";
 
 // Router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
